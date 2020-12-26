b5e6516de7157e6cf67cfbe7ebf171a4dab617d6
laundry-man <rnmkr@naver.com>
2020-12-26 12:51:22 +0900

    [2020-12-26] 레이아웃 구성 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import './static/css/App.css';
 import PullToRefresh from 'react-simple-pull-to-refresh';
+
 import Forky from './static/image/forky.jpg';
 import BrownHands from './static/image/brownhands.jpg';
 import Pic from './static/image/pic.jpg';
 import Night from './static/image/night-1846734_1920.jpg';
+
+import Maps from './static/icon/008-maps.svg';
+import Browser from './static/icon/036-browser.svg';
+import Notes from './static/icon/010-notes.svg';
+import Settings from './static/icon/025-settings.svg';
+import Books from './static/icon/024-books.svg';
 
 const DEFAULT_VALUES = {
   isPullable: true,
@@ -40,21 +47,20 @@ const App: React.FC = () => {
     });
   };
 
-  const [pullingContent, setPllingContent] = useState<string>('');
-  const [backgroundColor, setBackgroundColor] = useState<string>('#F2F1ED');
-
   return (
     <div className="App">
       <div className="caker-header">
-        <div className="caker-side">1</div>
-        <div className="caker-center">1</div>
-        <div className="caker-side">2</div>
+        <div className="caker-side"></div>
+        <div className="caker-center">
+          <div>CAKER</div>
+          <div style={{background: '#333333', borderRadius: '5px', height: '2.5px', margin: '4px 1px 6px 1px'}}>&nbsp;</div>
+        </div>
+        <div className="caker-side"></div>
       </div>
       <div className="caker-body">
         <div className="caker-side"></div>
         <div className="caker-center">
         <PullToRefresh
-          pullingContent={pullingContent}
           onRefresh={getNewData}
           canFetchMore={canFetchMore}
           isPullable={isPullable}
@@ -64,19 +70,19 @@ const App: React.FC = () => {
           maxPullDownDistance={maxPullDownDistance}
           className={"pull-to-refresh"}>
           <div className="caker-container">
-            <div style={{ width: '100%', margin: '10px' }}>
+            <div style={{ width: '100%', marginBottom: '10px' }}>
               <img alt="" src={Forky} style={{ height: '100%', width: '100%', borderRadius: '5px' }}></img>
             </div>
-            <div style={{ width: '100%', margin: '10px' }}>
+            <div style={{ width: '100%', marginBottom: '10px' }}>
               <img alt="" src={BrownHands} style={{ height: '100%', width: '100%', borderRadius: '5px' }}></img>
             </div>
-            <div style={{ width: '100%', margin: '10px' }}>
+            <div style={{ width: '100%', marginBottom: '10px' }}>
               <img alt="" src={Pic} style={{ height: '100%', width: '100%', borderRadius: '5px' }}></img>
             </div>
-            <div style={{ width: '100%', margin: '10px' }}>
+            <div style={{ width: '100%', marginBottom: '10px' }}>
               <img alt="" src={Night} style={{ height: '100%', width: '100%', borderRadius: '5px' }}></img>
             </div>
-            <div style={{ width: '100%', margin: '10px' }}>
+            <div style={{ width: '100%', marginBottom: '10px' }}>
               <img alt="" src={BrownHands} style={{ height: '100%', width: '100%', borderRadius: '5px' }}></img>
             </div>
           </div>
@@ -85,9 +91,17 @@ const App: React.FC = () => {
         <div className="caker-side"></div>
       </div>
       <div className="caker-footer">
-        <div className="caker-side">1</div>
-        <div className="caker-center"></div>
-        <div className="caker-side">2</div>
+        <div className="caker-side"></div>
+        <div className="caker-center">
+          <div style={{background: '#333333', borderRadius: '5px', height: '2.5px', margin: '6px 1px 12px 1px'}}>&nbsp;</div>
+          <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+            <div style={{width:'20vw'}}><img src={Maps} width='35%'></img></div>
+            <div style={{width:'20vw'}}><img src={Notes} width='35%'></img></div>
+            <div style={{width:'20vw'}}><img src={Books} width='35%'></img></div>
+            <div style={{width:'20vw'}}><img src={Settings} width='35%'></img></div>
+          </div>
+        </div>
+        <div className="caker-side"></div>
       </div>
     </div>
   );
