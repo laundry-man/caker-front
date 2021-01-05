eaecdc402055ab6de11271430a6ca03a92522322
laundry-man <rnmkr@naver.com>
2021-01-05 21:53:52 +0900

    [2020-01-05] 메인 UI 수정

--- src/dynamic/View.tsx
+++ src/dynamic/View.tsx
@@ -1,7 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ViewProps } from '../const/Types';
+import Matin1 from '../static/image/matin_1.png';
 
 function View({ path }: ViewProps) {
+    const [toggle, setToggle] = useState(false);
+    const [timer, setTimer] = useState<NodeJS.Timeout>();
+
+    const [container, setContainer] = useState<JSX.Element>(<></>);
+
     const [active, setActive] = useState(() => {
         let _active: boolean[] = [];
         for (let i = 0; i < path.length; i++)
@@ -9,18 +15,48 @@ function View({ path }: ViewProps) {
         return _active;
     });
 
+    const getViewSize = (width: number, height: number) => {
+        setContainer(
+            <div className="view-container" style={{width: {width} + 'px', height: {height} + 'px'}}></div>
+        );
+    };
+
     const getNextView = () => {
+        if (toggle) {
+
+        }
+        else {
+            setToggle(true);
+        }
+    }
+
+    useEffect(() => {
+        if (toggle) {
+            setTimer(
+                setTimeout(()=>{
+                    setToggle(false)
+                }, 3000)
+            );
+        }
+    }, [toggle]);
+
+    /*const getNextView = () => {
         let _active: boolean[] = [...active];
         let _f: boolean | undefined = _active.pop();
         _active.unshift(_f === undefined ? false : _f);
         setActive(_active);
-    };
+    };*/
 
     return (
         <div className="view-wrapper" onClick={getNextView}>
-            {active.map((active, index) => {
-                return (<img key={index} className={active ? "view fade-in-fast" : "invisible"} alt="" src={path[index]}></img>);
-            })}
+            {!toggle ?
+                <>
+                <div className="view-tag">#고래상점downtown</div>
+                <img className="view-image" alt="" src={Matin1} 
+                    onLoad={(e)=>{getViewSize(e.currentTarget.width, e.currentTarget.height)}}/>
+                </> :
+                {container}
+            }
         </div>
     );
 }
