ece2f87ff8ef9ab66063988b20147293def5c313
laundry-man <rnmkr@naver.com>
2021-01-15 20:42:18 +0900

    [2021-01-15] 게시글 업로드 화면 구성 1

--- src/dynamic/Upload.tsx
+++ src/dynamic/Upload.tsx
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 
+import '../static/css/upload.css';
+
+import Tux from '../static/image/Tux.png';
+
 function Upload() {
     return (
-        <div>&nbsp;</div>
+        <div className="caker-container">
+            <div className="upload-button fade-in-slow">
+                <div className="upload-button-content-1">please</div>
+                <div className="upload-button-content-2">touch</div>
+                <div className="upload-button-content-3">me.</div>
+                <img className="upload-button-content-4" src={Tux} alt=""></img>
+            </div>
+        </div>
     );
 }
 
