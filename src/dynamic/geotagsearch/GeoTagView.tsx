159a365555aabe286f903508f435a0843a20e1f0
laundry-man <rnmkr@naver.com>
2021-04-05 14:55:57 +0900

    [2021-04-05] VIEW 스타일 수정

--- src/dynamic/geotagsearch/GeoTagView.tsx
+++ src/dynamic/geotagsearch/GeoTagView.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import { Page, GEO_TAG_SEARCH, TAG_SEARCH_RESULT } from '../../const/Constant';
 
+import classNames from 'classnames';
 import index from '../../static/css/index.module.css';
 import geoTagView from '../../static/css/geotagsearch/geoTagView.module.css';
 
@@ -18,10 +19,6 @@ function GeoTagView({
     setContent,
     setPredecessor }: GeoTagViewProps) {
 
-    const [width, setWidth] = useState(0);
-    const [height, setHeight] = useState(0);
-
-    const [isLoaded, setIsLoaded] = useState(false);
     const [toggle, setToggle] = useState(false);
     const [timer, setTimer] = useState<NodeJS.Timeout>();
 
@@ -33,7 +30,7 @@ function GeoTagView({
             setPredecessor(GEO_TAG_SEARCH);
             redirect(TAG_SEARCH_RESULT);
         }
-        else if (isLoaded) {
+        else {
             setToggle(true);
             setTimer(
                 setTimeout(() => {
@@ -43,26 +40,14 @@ function GeoTagView({
         }
     }
 
-    function getImageSize(width: number, height: number) {
-        setWidth(width);
-        setHeight(height);
-        setIsLoaded(true);
-    };
-
     return (
-        <div className={geoTagView.wrapper} onClick={getNextView}>
+        <div onClick={getNextView}>
             {toggle ? 
                 <BackView 
                     path={path} 
-                    width={width} 
-                    height={height}
                 /> : 
                 <FrontView 
                     path={path}
-                    width={width}
-                    height={height}
-                    isLoaded={isLoaded}
-                    getImageSize={getImageSize}
                 />
             }
         </div>
@@ -70,61 +55,49 @@ function GeoTagView({
 }
 
 type FrontViewProps = {
-    path: string,
-    width: number,
-    height: number,
-    isLoaded: boolean,
-    getImageSize: (width: number, height: number) => void
+    path: string
 };
 
-function FrontView({ 
-    path, 
-    width, 
-    height, 
-    isLoaded,
-    getImageSize}: FrontViewProps) {
+function FrontView({ path }: FrontViewProps) {
 
     return (
-        <div className={index.fadeInFast} 
-            style={{ 
-                width: isLoaded ? width : 'auto', 
-                height: isLoaded ? height : 'auto'
-            }}>
-            <div className={geoTagView.dotWrapper}>
-                <div className={geoTagView.dot}>●</div>
+        <div className={classNames([geoTagView.view, index.fadeInFast])} style={{ backgroundImage: 'url(' + path + ')'}}>
+            <div className={geoTagView.header}>
+                <div className={geoTagView.prepend}>&nbsp;</div>
+                <div className={geoTagView.append}>
+                    <div className={geoTagView.dot1}>
+                        ●
+                    </div>
+                </div>
             </div>
-            <img className={geoTagView.image} alt="" src={path}
-                onLoad={(e) => { 
-                    if (!isLoaded) 
-                        getImageSize(e.currentTarget.width, e.currentTarget.height); 
-                }} 
-            />
         </div>
     );
 }
 
 type BackViewProps = {
     path: string,
-    width: number,
-    height: number
 };
 
-function BackView({ 
-    path, 
-    width, 
-    height }: BackViewProps) {
+function BackView({ path }: BackViewProps) {
 
     return (
-        <div className={index.fadeInFast}>
-            <div className={geoTagView.dotWrapper}>
-                <div className={geoTagView.dot}>●</div>
-            </div>
-            <div className={geoTagView.detailWrapper} 
-                style={{ width: width, height: height, backgroundImage: 'url(' + path + ')' }}>
-                <div className={geoTagView.detail} 
-                    style={{ width: width, height: height }}>
+        <div className={classNames([geoTagView.view, index.fadeInFast])} style={{ backgroundImage: 'url(' + path + ')'}}>
+            <div className={geoTagView.backdrop}>
+                <div className={geoTagView.header}>
+                    <div className={geoTagView.prepend}>&nbsp;</div>
+                    <div className={geoTagView.append}>
+                        <div className={geoTagView.dot2}>
+                            ●
+                        </div>
+                    </div>
+                </div>
+                <div className={geoTagView.info}>
                     <div className={geoTagView.tag}>#고래상점</div>
-                    <div className={geoTagView.distance}>1.2KM 44K</div>
+                    <div className={geoTagView.measured}>
+                        <span>1.2km</span>
+                        <span>&nbsp;</span>
+                        <span>44k</span>
+                    </div>
                 </div>
             </div>
         </div>
