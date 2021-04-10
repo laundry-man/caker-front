401aac05eff4150de8a9d735eb0d6eddc59cf7fb
laundry-man <rnmkr@naver.com>
2021-04-10 16:53:36 +0900

    [2021-04-10] 이미지 그룹 블록 생성 규칙 정의

---
+++ src/dynamic/postupload/FrontView.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+import Vibe from './Vibe';
+import ImageGroup from './ImageGroup';
+
+import index from '../../static/css/index.module.css';
+import frontView from '../../static/css/postupload/frontView.module.css';
+
+type FrontViewProps = {
+    viewIndex: number,
+    vibeIndex: number,
+    vibeList: string[],
+    shrinkFloor: number,
+    blockList: number[],
+    blockTouchEvent: (blockIndex: number, blockType: number) => void,
+    setVibeIndex: React.Dispatch<React.SetStateAction<number>>
+};
+
+function FrontView({ 
+    viewIndex,
+    vibeIndex,
+    vibeList,
+    shrinkFloor,
+    blockList, 
+    blockTouchEvent,
+    setVibeIndex }: FrontViewProps) {
+
+    const [isStretch, setIsStretch] = useState(false);
+
+    return (
+        <div className={viewIndex === 0 ? index.fadeInSlow : index.nonDisplay}>
+            <Vibe 
+                vibeIndex={vibeIndex} 
+                vibeList={vibeList}
+                setVibeIndex={setVibeIndex} 
+                isStretch={isStretch} 
+                setIsStretch={setIsStretch} 
+            />
+            <div className={frontView.separator} style={{height: isStretch ? '0.3vh' : '3.5vh'}}/>
+            <ImageGroup 
+                shrinkFloor={shrinkFloor}
+                blockList={blockList}
+                blockTouchEvent={blockTouchEvent}
+            />
+        </div>
+    );
+}
+
+export default FrontView;
